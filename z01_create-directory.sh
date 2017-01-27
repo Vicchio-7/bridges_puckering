a5a960abdfbc0c80856810a1db001a63939da32f
Vicchio-7 <vicchio@umich.edu>
2017-01-27 14:57:42 -0500

    working

--- z01_create-directory.sh
+++ z01_create-directory.sh
@@ -23,7 +23,6 @@ user=vicchio
 ## Additional Required Information ##
 # Additional information such as folder location that is required for the code to run properly.
 
-
 tpl=${p2}/y_template_files
 p1=/pylon1/${account}/${user}
 p2=/pylon2/${account}/${user}
@@ -53,14 +52,31 @@ if [ ${status_build} == 1 ]; then
 elif [ ${status_build} == 0 ] ; then
     echo 'Ready to rumble!'
 
-    directory=${p2}/${folder}/${level_short}
+    directory=${p2}/puckering/${folder}/${level_short}
 
     if [ -d ${directory} ] ; then
 		echo
 		echo "This directory already exists for ${level_of_theory_short} in ${folder}..."
 		echo
 	else
+	    echo
+	    echo "The following directory is being made for $level_short in $molecule_type"
+	    echo
+
 	    mkdir $directory
+
+	    cd ${directory}
+
+	    mkdir 0_initial-coordinates
+	    mkdir 1_sample-files
+	    mkdir 2__freeze
+	    mkdir 3_relax-freeze_run
+	    mkdir 4_opt_localmin
+	    mkdir 5_opt_TS
+	    mkdir 6_norm_analysis
+	    mkdir 7_irc_run
+		mkdir 8_irc_localmin
+
     fi
 
 fi
