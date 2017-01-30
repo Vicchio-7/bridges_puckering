aa69caeb1ad77a54a9b55f26a8e11648e8cdb26a
Vicchio-7 <vicchio@umich.edu>
2017-01-30 14:10:28 -0500

    working

--- z00_submit-batch.sh
+++ z00_submit-batch.sh
@@ -1,1 +1,23 @@
-echo 'This script will submit a bunch of jobs on Bridges!'
+#!/usr/bin/env bash
+
+# Created by: Stephen P. Vicchio
+#
+# This script submits all slurm files in a directory for rapid submission on PSC.
+#
+
+# --------------------------------------------------------------------------------------
+
+## Input - Command Line ##
+# The following information is needed from the command line
+
+run_type=$1
+keyword=$2
+
+# --------------------------------------------------------------------------------------
+
+## Main Code ##
+
+for file_sub in $(find . \( ! -name . -prune \) -type f -iname 'slurm-*.job'); do
+		sbatch ${file_sub}
+done
+
