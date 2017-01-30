18584e67ce980ed4e68b89123ed03184e8be1929
Vicchio-7 <vicchio@umich.edu>
2017-01-30 09:53:37 -0500

    testing

--- z12_local_min.sh
+++ z12_local_min.sh
@@ -63,11 +63,41 @@ elif [ ${status_build} == 0 ] ; then
 
     dir_job=${directory}/${folder_type}
 
-    cd ${dir_job}
+    if [ ${molecule} == "oxane" ] ; then
 
-    echo ${directory}
-    echo ${dir_job}
+        for file_unedit in $( <$input_list); do
 
+            file=${file_unedit%.com}
 
+            echo ${file}
+
+#        ######## The section below updates the Gaussian Input File
+#
+#            sed -e "s/\$memory/${total_memory}/g" $tpl/${template_file_oxane} > temp1.com
+#            sed -e "s/\$num_procs/${cores_per_node}/g" temp1.com >> temp2.com
+#            sed -e "s/\$folder_old/${1}-freeze_${3}/g" temp2.com >> temp3.com
+#            sed -e "s/\$old_check/${1}-${file}-freeze_${3}.chk/g" temp3.com >> temp4.com
+#            sed -e "s/\$chkfile/${molecule}-${file}-freeze_${short_level_of_theory}-${test_type}_${short_level_of_theory}.chk/g" temp4.com >> temp5.com
+#            sed -e "s/\$folder/${molecule}-${test_type}_${3}/g" temp5.com > temp6.com
+#            sed -e "s/level_of_theory/${level_of_theory}/g" temp6.com >> temp7.com
+#
+#            mv temp7.com ${file}.com
+#
+#            rm temp*.com
+#
+#        ######## The section below creates the PBS file for submission on flux
+#
+#            sed -e "s/molecule/${molecule}/g" $tpl/gaussian-PBS-tpl.txt > temp1.txt
+#            sed -e "s/study/${2}/g" temp1.txt >> temp2.txt
+#            sed -e "s/level_of_theory_short/${short_level_of_theory}/g" temp2.txt >> temp3.txt
+#            sed -e "s/conform/${file}/g" temp3.txt >> temp4.txt
+#            sed -e "s/gauss-log/${1}-${file}-freeze_${3}-${2}_${3}/g" temp4.txt >> temp5.txt
+#            sed -e "s/\$num_proc/${cores_per_node}/g" temp5.txt >> temp6.txt
+#            sed -e "s/\$pmemory/${memory_job}/g" temp6.txt >> temp7.txt
+#
+#            mv temp7.txt PBS-${2}-${file}.txt
+#
+#            rm temp*.txt
+        done
 
 fi
