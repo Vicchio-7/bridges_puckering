9e4a2e118b7e82e9df52799e6f54173940d3fec8
Vicchio-7 <vicchio@umich.edu>
2017-01-30 21:16:26 -0500

    freeze

--- z11_freeze_jobs.sh
+++ z11_freeze_jobs.sh
@@ -16,6 +16,13 @@
 molecule_type=$1
 level_short=$2
 
+## Input - Gaussian Run Information ##
+# The following information determines the numbers of cores and memory the jobs will require.
+cores_per_node=1
+memory_job=15
+hours=2 #1, 2 ,3 ..... 10, 11, 12....
+minutes=45 # number between 0 and 59
+
 ## Input - Codes ##
 # Please update the following input commands depending on the user.
 
