7f1fa05bda1ce11c2762ff770488c1f9adea16d0
Vicchio-7 <vicchio@umich.edu>
2017-01-30 20:39:23 -0500

    freeze

--- z00_submit-batch.sh
+++ z00_submit-batch.sh
@@ -18,16 +18,16 @@ keyword=$2
 ## Main Code ##
 
 if [[ ${run_type} == 'rerun' ]]; then
-	for file_sub in $(find . \( ! -name . -prune \) -type f -iname 'PBS-*_rerun*'); do
+	for file_sub in $(find . \( ! -name . -prune \) -type f -iname 'slurm-*_rerun*'); do
         sbatch ${file_sub}
 	done
 elif [[ ${run_type} == 'none' ]]; then
     keyword=$2
-	for file_sub in $(find . \( ! -name . -prune \) -type f -iname "PBS-*${keyword}*"); do
+	for file_sub in $(find . \( ! -name . -prune \) -type f -iname "slurm-*${keyword}*.job"); do
         sbatch ${file_sub}
 	done
 else
-	for file_sub in $(find . \( ! -name . -prune \) -type f -iname 'PBS-*'); do
+	for file_sub in $(find . \( ! -name . -prune \) -type f -iname 'slurm-*.job'); do
 		sbatch ${file_sub}
 	done
 fi
