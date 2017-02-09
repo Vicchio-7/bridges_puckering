07b1418d38721dcb63fe45ff83e62225cf500759
Vicchio-7 <vicchio@umich.edu>
2017-02-09 12:03:42 -0500

    testing

--- z15_irc_run.sh
+++ z15_irc_run.sh
@@ -112,11 +112,12 @@ elif [ ${status_build} == 0 ] ; then
                 sed -e "s/\$memory/${total_memory}/g" ${tpl}/${tpl_folder}/run_irc_forward.tpl > temp1.temp
                 sed -i "s/\$num_procs/${cores_per_node}/g" temp1.temp
                 sed -i "s/\$folder_1/${folder}/g" temp1.temp
-                sed -i "s/\$folder_new/${irc_forward}/g" temp1.temp
-                sed -i "s/\$chkfile/${file}-freeze_${level_short}-${job_type}_${level_short}.chk/g" temp1.temp
+                sed -i "s/\$folder_new/${1}-${2}_${3}-forward/g" temp1.temp
+                sed -i "s/\$old_check/${file_org}.chk/g" temp1.temp
+                sed -i "s/\$chkfile/${new_check_file}/g" temp1.temp
                 sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp
 
-            mv temp1.temp ${file}.com
+                mv temp1.temp ${new_filenamef}.com
 
                 ######## The section below creates the Slurm file for submission on Bridges
                 sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_slurm_script.job > temp1.txt
