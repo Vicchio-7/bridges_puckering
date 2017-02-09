2107ecaaaac9e6a4a8febf81b732f3b416f7b426
Vicchio-7 <vicchio@umich.edu>
2017-02-09 12:14:26 -0500

    should work

--- z15_irc_run.sh
+++ z15_irc_run.sh
@@ -121,14 +121,14 @@ elif [ ${status_build} == 0 ] ; then
                 mv temp1.temp ${new_filenamef}.com
 
                 ######## The section below creates the Slurm file for submission on Bridges
-#                sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_slurm_script.job > temp1.txt
-#                sed -i "s/conform/${file_org}/g" temp1.txt
-#                sed -i "s/gauss-log/${new_filenamef}/g" temp1.txt
-#                sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
-#                sed -i "s/\$test/${job_type}/g" temp1.txt
-#                sed -i "s/\$level/${level_short}/g" temp1.txt
-#                sed -i "s/\$hours/${hours}/g" temp1.txt
-#                sed -i "s/\$minutes/${minutes}/g" temp1.txt
+                sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_slurm_script.job > temp1.txt
+                sed -i "s/conform/${file_org}/g" temp1.txt
+                sed -i "s/gauss-log/${new_filenamef}/g" temp1.txt
+                sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
+                sed -i "s/\$test/${job_type}/g" temp1.txt
+                sed -i "s/\$level/${level_short}/g" temp1.txt
+                sed -i "s/\$hours/${hours}/g" temp1.txt
+                sed -i "s/\$minutes/${minutes}/g" temp1.txt
 #
 #                mv temp1.txt slurm-${new_filenamef}.job
 
