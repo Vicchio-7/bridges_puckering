fb36057806a4f320accf1f93243c68eeb2533d06
Vicchio-7 <vicchio@umich.edu>
2017-02-09 19:52:23 -0500

    push

--- z22_run_hartree_xyz_optall.sh
+++ z22_run_hartree_xyz_optall.sh
@@ -75,12 +75,12 @@ elif [ ${status_build} == 0 ] ; then
             hartree cpsnap -d $PWD > z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
             z05_grab_xyz_coords.sh ${molecule_type}
             xyz_cluster -s z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv -t ${tol}
-            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_single_cluster_sorted-${job_type}-${molecule_type}-${level_short}.csv
+            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_single_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
         else
             hartree cpsnap -d $PWD > z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
             z05_grab_xyz_coords.sh ${molecule_type}
             xyz_cluster -s z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv -t ${tol}
-            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_single_cluster_sorted-${job_type}-${molecule_type}-${level_short}.csv
+            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_single_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
         fi
 
     fi
@@ -94,7 +94,7 @@ elif [ ${status_build} == 0 ] ; then
     echo
 
     cp z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
-    cp z_single_cluster_sorted-${job_type}-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/z_single_cluster_sorted-${job_type}-${molecule_type}-${level_short}.csv
+    cp z_single_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/z_single_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
 
     echo "Copied all log files to 9_al_lm_logs"
     echo
