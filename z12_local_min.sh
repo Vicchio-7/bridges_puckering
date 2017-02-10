8ca23bc93b26621647dbdee330cd1a83629aff31
Vicchio-7 <vicchio@umich.edu>
2017-02-09 19:16:32 -0500

    go go go

--- z12_local_min.sh
+++ z12_local_min.sh
@@ -143,7 +143,7 @@ elif [ ${status_build} == 0 ] ; then
             sed -i "s/\$num_procs/${cores_per_node}/g" temp1.temp
             sed -i "s/\$folder_1/${folder}/g" temp1.temp
             sed -i "s/\$folder_new/${molecule_type}-optall_${level_short}/g" temp1.temp
-            sed -i "s/\$chkfile/${file}-freeze_${level_short}-${job_type}_${level_short}.chk/g" temp1.temp
+            sed -i "s/\$chkfile/${file}-${job_type}_${level_short}.chk/g" temp1.temp
             sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp
 
             mv temp1.temp ${file}.com
