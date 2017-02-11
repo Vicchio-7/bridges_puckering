123c290be60c8564eef759d2bf75908f546b728f
Vicchio-7 <vicchio@umich.edu>
2017-02-11 17:52:14 -0500

    GO BLUE

--- z14_norm_analysis.sh
+++ z14_norm_analysis.sh
@@ -100,10 +100,11 @@ elif [ ${status_build} == 0 ] ; then
         sed -e "s/\$memory/${total_memory}/g" ${tpl_file} > temp1.temp
         sed -i "s/\$num_procs/${cores_per_node}/g" temp1.temp
         sed -i "s/\$folder_1/${folder}/g" temp1.temp
-        sed -i "s/\$folder_new/${molecule_type}-TS_${level_short}/g" temp1.temp
+        sed -i "s/\$folder_old/${molecule_type}-TS_${level_short}/g" temp1.temp
+        sed -i "s/\$folder_new/${molecule_type}-norm_${level_short}/g" temp1.temp
         sed -i "s/\$chkfile/${file_org}-norm_${3}.chk/g" temp1.temp
         sed -i "s/\$old_check/${file_org}.chk/g" temp1.temp
-        sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp > ${file_org}.com
+        sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp
 
         mv temp1.temp ${file_org}.com
 
