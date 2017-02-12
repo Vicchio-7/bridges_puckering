c73c266a5a6518e4c5449a6a657d56eebbf5e469
Vicchio-7 <vicchio@umich.edu>
2017-02-12 18:24:06 -0500

    running

--- z24_norm_analysis.sh
+++ z24_norm_analysis.sh
@@ -118,11 +118,11 @@ elif [ ${status_build} == 0 ] ; then
     z05_grab_xyz_coords.sh ${molecule_type}
     xyz_cluster -s ${new_dir}/z_hartree_ring_pucker-unsorted-TS-${level_short}.csv -t ${tol}
 
-#    mv z_cluster_z_hartree_ring_pucker-unsorted-TS-${level_short}.csv z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv
-#
-#    cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ../.
-#    cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/.
-#    cp z_norm-analysis_TS-${level_short}_ring_puckers.txt ${results_location}/${folder}/${level_short}/.
+    mv z_cluster_z_hartree_ring_pucker-unsorted-TS-${level_short}.csv z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv
+
+    cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ../.
+    cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/.
+    cp z_norm-analysis_TS-${level_short}_ring_puckers.txt ${results_location}/${folder}/${level_short}/.
 
     fi
 fi
