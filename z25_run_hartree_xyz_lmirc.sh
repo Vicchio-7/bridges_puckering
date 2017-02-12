313219cabb3a92338ca119a2972f6dc6ca418ebf
Vicchio-7 <vicchio@umich.edu>
2017-02-12 17:13:21 -0500

    sending

--- z25_run_hartree_xyz_lmirc.sh
+++ z25_run_hartree_xyz_lmirc.sh
@@ -2,7 +2,7 @@
 
 # Created by: Stephen P. Vicchio
 
-# This script performs the local min optimization for the
+# This scripts run hartree for the lmirc jobs
 #
 # The code is divided into a few section; if you are not Stephen Vicchio, please be sure
 # to change the '## Input - Command Line ##' options (first section below).
@@ -61,7 +61,7 @@ if [ ${status_build} == 1 ]; then
 	exit
 elif [ ${status_build} == 0 ] ; then
 
-    z04_check_normal_termination.sh ${molecule_type} TS ${level_short}
+    z04_check_normal_termination.sh ${molecule_type} lmirc ${level_short}
 
     if [ ! -f ${failure} ]; then
         echo "No Files failed! Performing hartree and xyz_cluster"
@@ -71,8 +71,14 @@ elif [ ${status_build} == 0 ] ; then
 
         if [[ ${molecule_type} == 'oxane' ]]; then
             hartree cpsnap -d $PWD > z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
+            z05_grab_xyz_coords.sh ${molecule_type}
+            xyz_cluster -s z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv -t ${tol}
+            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
         else
             hartree cpsnap -d $PWD > z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
+            z05_grab_xyz_coords.sh ${molecule_type}
+            xyz_cluster -s z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv -t ${tol}
+            mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
         fi
 
     fi
@@ -86,10 +92,11 @@ elif [ ${status_build} == 0 ] ; then
     echo
 
     cp z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
+    cp z_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv z_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
 
-    echo "Copied all log files to 9_al_lm_logs"
-    echo
-
-    cp *.log ../9_all_lm_logs/.
+#    echo "Copied all log files to 10_ts_viz_logs"
+#    echo
+#
+#    cp *.log ../10_ts_viz_logs/.
 
 fi
