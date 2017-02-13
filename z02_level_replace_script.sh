41669e16024d8d732cb5f82a684831bb026d3743
Vicchio-7 <vicchio@umich.edu>
2017-02-13 13:26:21 -0500

    sending:

--- z02_level_replace_script.sh
+++ z02_level_replace_script.sh
@@ -25,6 +25,9 @@ if [ "${molecule_type}" == 'oxane' ] ; then
 elif [ "${molecule_type}" == 'bxyl' ] ;  then
 	folder=2_bxyl
 	status_build=0
+elif [ "${molecule_type}" == 'bglc' ] ;  then
+	folder=3_betagluc
+	status_build=0
 else
 	echo
 	echo "The molecule type is not found in this script"
