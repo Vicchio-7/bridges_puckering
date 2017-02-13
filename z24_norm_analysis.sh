b37cf080c1d379a00eaee36c12b7c658753e63c1
Vicchio-7 <vicchio@umich.edu>
2017-02-13 14:13:27 -0500

    trying

--- z24_norm_analysis.sh
+++ z24_norm_analysis.sh
@@ -83,8 +83,6 @@ elif [ ${status_build} == 0 ] ; then
 
     norm_analysis -s z_list_norm_files.txt -r ${ring_atoms}
 
-
-
     mv z_norm-analysis_TS_exo_puckers_z_list_norm_files.txt z_norm-analysis_TS-${level_short}_exo_puckers.txt
     mv z_norm-analysis_TS_ring_puckers_z_list_norm_files.txt z_norm-analysis_TS-${level_short}_ring_puckers.txt
 
