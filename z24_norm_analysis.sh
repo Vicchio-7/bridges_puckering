9d3fe7030df92da3ce439a5fc6b626d2dc3f159e
Vicchio-7 <vicchio@umich.edu>
2017-02-22 10:20:47 -0500

    fixing

--- z24_norm_analysis.sh
+++ z24_norm_analysis.sh
@@ -117,7 +117,8 @@ elif [ ${status_build} == 0 ] ; then
 
     dipole_cluster -s ${new_dir}/z_hartree_ring_pucker-unsorted-TS-${molecule_type}-${level_short}.csv -t ${tol}
 
-    cp z_hartree_ring_pucker-unsorted-TS-${molecule_type}-${level_short}.csv z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv
+
+    mv z_cluster_z_hartree_ring_pucker-unsorted-TS-${molecule_type}-${level_short}.csv z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv
 
     cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ../.
     cp z_cluster_ring_pucker-sorted-TS-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/.
