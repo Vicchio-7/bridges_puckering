e141567e496424b5b6698c153b25ea733a4b0672
Vicchio-7 <vicchio@umich.edu>
2017-02-26 14:12:24 -0500

    sending

--- z23_run_hartree_xyz_TS.sh
+++ z23_run_hartree_xyz_TS.sh
@@ -40,7 +40,7 @@ failure=out-failure-${1}-${2}-${3}.status
 ## Setup Check ##
 if [ "${molecule_type}" == 'oxane' ] ; then
 	folder=1_oxane
-    tol=0.1
+    tol=0.01
 	status_build=0
 elif [ "${molecule_type}" == 'bxyl' ] ;  then
 	folder=2_bxyl
@@ -79,6 +79,7 @@ elif [ ${status_build} == 0 ] ; then
             z05_grab_xyz_coords.sh ${molecule_type}
             xyz_cluster -s z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv -t ${tol}
             mv z_cluster_z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv z_cluster-sorted-${job_type}-${molecule_type}-${level_short}.csv
+            cp z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv ${results_location}/${folder}/${level_short}/z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
         else
             hartree cpsnap -d $PWD > z_hartree-unsorted-${job_type}-${molecule_type}-${level_short}.csv
             z05_grab_xyz_coords.sh ${molecule_type}
