d60c2c32bb1a60724018a08cee827dea624cd583
Vicchio-7 <vicchio@umich.edu>
2017-03-21 13:31:41 -0400

    sending

--- z19_dftb_runs.sh
+++ z19_dftb_runs.sh
@@ -184,7 +184,7 @@ elif [ ${status_build} == 0 ] ; then
 
             sed -i "6r ${dftb_ending}" ${file}.com
 
-            sed -i '16i\n' ${file}.com
+            sed -i '16s/$/\n/' ${file}.com
 
             #sed -i '16i/$/\n' ${file}.com
 
