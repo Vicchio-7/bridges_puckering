c7ef74a43264eb1f796b3e11b74d7c8491c3ea26
Vicchio-7 <vicchio@umich.edu>
2017-05-23 11:43:03 -0400

    sending

--- z16_local_min_rerun.sh
+++ z16_local_min_rerun.sh
@@ -84,11 +84,39 @@ elif [ ${status_build} == 0 ] ; then
         mkdir ${p2}/puckering/${folder}/${level_short}/${new_folder}/
     fi
 
-    input_list=$(ls ${p2}/puckering/${folder}/${level_short}/9_all_lm_logs/*.log)
+    input_list=$(ls ${p2}/puckering/${folder}/${level_short}/9_all_lm_logs/*.xyz)
 
     echo ${input_list}
 
+    for file_unedit in $( <$input_list); do
+        file=${file_unedit%.xyz}
 
+        echo ${file}
 
+#    ######## The section below updates the Gaussian Input File
+#        sed -e "s/\$memory/${total_memory}/g" ${tpl}/${tpl_folder}/run_bxyl_prefrozen_optall-to-TS.tpl > temp1.temp
+#        sed -i "s/\$num_procs/${cores_per_node}/g" temp1.temp
+#        sed -i "s/\$folder_1/${folder}/g" temp1.temp
+#        sed -i "s/\$folder_new/${molecule_type}-TS_${level_short}/g" temp1.temp
+#        sed -i "s/\$folder_old/${molecule_type}-freeze_${level_short}/g" temp1.temp
+#        sed -i "s/\$old_check/${file}-freeze_${level_short}.chk/g" temp1.temp
+#        sed -i "s/\$chkfile/${file}-${job_type}_${level_short}.chk/g" temp1.temp
+#        sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp
+#
+#        mv temp1.temp ${file}.com
+#
+#    ######## The section below creates the Slurm file for submission on Bridges
+#        sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_slurm_script.job > temp1.txt
+#        sed -i "s/conform/${file}/g" temp1.txt
+#        sed -i "s/gauss-log/${file}-${2}_${3}/g" temp1.txt
+#        sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
+#        sed -i "s/\$test/${job_type}/g" temp1.txt
+#        sed -i "s/\$level/${level_short}/g" temp1.txt
+#        sed -i "s/\$hours/${hours}/g" temp1.txt
+#        sed -i "s/\$minutes/${minutes}/g" temp1.txt
+#
+#        mv temp1.txt slurm-${file}.job
+
+    done
 fi
 
