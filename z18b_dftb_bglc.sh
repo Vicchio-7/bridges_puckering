96c94a9418f1b02e9bc0ae1c403a7351b9cd36ad
Vicchio-7 <vicchio@umich.edu>
2017-09-18 19:22:28 -0400

    sending

--- z18b_dftb_bglc.sh
+++ z18b_dftb_bglc.sh
@@ -176,16 +176,19 @@ elif [ ${status_build} == 0 ] ; then
                 log_id=${file}-freeze_dftb3-TS_dftb3
             fi
         elif [ "${job_type}" == 'norm' ] ; then
-            echo ${file}
-            sed -e "s/\$memory/${total_memory}/g" ${tpl_file} > ${file}.com
-            sed -i "s/\$num_procs/${cores_per_node}/g" ${file}.com
-            sed -i "s/\$folder_old/${molecule_type}-freeze_${level_short}/g" ${file}.com
-            sed -i "s/\$folder_1/${folder}/g" ${file}.com
-            sed -i "s/\$old_check/${file}-TS_${level_short}.chk/g" ${file}.com
-            sed -i "s/\$folder_new/${molecule_type}-${job_type}_${level_short}/g" ${file}.com
-            sed -i "s/\$chkfile/${file}-${job_type}_${level_short}.chk/g" ${file}.com
-            slurm_build=0
-            log_id=${file}-freeze_dftb3-TS_dftb3-norm_dftb3
+            job_number=${file#${remove_molecule}}
+            if (( ${job_number} >= ${ts_number} )); then
+                echo ${file}
+                sed -e "s/\$memory/${total_memory}/g" ${tpl_file} > ${file}.com
+                sed -i "s/\$num_procs/${cores_per_node}/g" ${file}.com
+                sed -i "s/\$folder_old/${molecule_type}-freeze_${level_short}/g" ${file}.com
+                sed -i "s/\$folder_1/${folder}/g" ${file}.com
+                sed -i "s/\$old_check/${file}-TS_${level_short}.chk/g" ${file}.com
+                sed -i "s/\$folder_new/${molecule_type}-${job_type}_${level_short}/g" ${file}.com
+                sed -i "s/\$chkfile/${file}-${job_type}_${level_short}.chk/g" ${file}.com
+                slurm_build=0
+                log_id=${file}-freeze_dftb3-TS_dftb3-norm_dftb3
+             fi
         elif [ "${job_type}" == 'irc' ] ; then
             echo 'irc'
         fi
